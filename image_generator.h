74f1a77ccfb4364c24d1207faff5ee4e559a1b9c
Samuel Messing <sam@bok030.wlan.business-services.upenn.edu>
2010-11-19 12:14:36 -0500

    working on image_generator headers.

---
+++ image_generator.h
@@ -0,0 +1,10 @@
+/** 
+* image_generator.h: For talking to camera and generating formatted
+*   images for use in vissynth.cpp
+* AUTHORS:
+* Chang
+* Evan
+* Sam (smessing@sas.upenn.edu)
+* Yui
+*/
+
