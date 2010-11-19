8892d40c2447289176d341d2eb1c4b948ce8d2d0
Samuel Messing <sam@bok030.wlan.business-services.upenn.edu>
2010-11-19 12:12:22 -0500

    starting outling of vissynth.

---
+++ vissynth.cpp
@@ -0,0 +1,12 @@
+/**
+* vissynth.cpp: Controller source for vissynth application.
+* AUTHORS:
+* Chang
+* Evan
+* Sam (smessing@sas.upenn.edu)
+* Yui
+*/
+
+#include "image_generator.h"
+#include "sound_generator.h"
+#include "view.h"
