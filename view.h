1a1f32452181b1321e390ebcfd70127b26d14a55
Samuel Messing <sam@bok030.wlan.business-services.upenn.edu>
2010-11-19 13:18:16 -0500

    adding methods to view.

--- view.h
+++ view.h
@@ -13,6 +13,9 @@ public:
 
     // Play an individual column from a bit_rep:
     void play_col(vector<bool> to_play);
+    
+    // Draw a bit rep to the screen:
+    void draw_rep(bit_rep *to_draw);
 };
 
 
